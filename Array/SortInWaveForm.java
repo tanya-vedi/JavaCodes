e906050537c811d64b008f80093b79be56ac05ac
Tanya Vedi <tanyavedi76@gmail.com>
2021-04-18 16:25:33 +0530

    Updated

--- Array/SortInWaveForm.java
+++ Array/SortInWaveForm.java
@@ -19,5 +19,18 @@ public class Solution {
                 swap(arr, i, i+1);
         }
 	}
+	
+	/*Alternatively :O(n^2)
+	// This function sorts arr[0..n-1] in wave form, i.e.,
+    // arr[0] >= arr[1] <= arr[2] >= arr[3] <= arr[4]..
+    void sortInWave(int arr[], int n)
+    {
+        // Sort the input array
+        Arrays.sort(arr);
+  
+        // Swap adjacent elements
+        for (int i=0; i<n-1; i += 2)
+            swap(arr, i, i+1);
+    }*/
 
 }
